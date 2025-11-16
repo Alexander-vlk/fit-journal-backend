57608fa3b0d9cd6130d54c91f11124cc0541c9c1
alex_barmen <www.lad282@gmail.com>
2025-11-16 20:50:58 +0300

    feat: athlete admin register

--- fit_journal/auth_service/admin.py
+++ fit_journal/auth_service/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from auth_service.models import Athlete
+
+@admin.register(Athlete)
+class AthleteAdmin(UserAdmin):
+    """Админ для модели Athlete"""
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (
+            'Personal info',
+            {
+                'fields': (
+                    'last_name',
+                    'first_name',
+                    'second_name',
+                    'phone',
+                    'email',
+                ),
+             },
+        ),
+        (
+            'Permissions',
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                ),
+            },
+        ),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
