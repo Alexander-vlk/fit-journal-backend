8ddc9acbca11138263d20586f42905aebf9b19d0
alex_barmen <www.lad282@gmail.com>
2025-11-21 21:44:02 +0300

    feat: admin

--- fit_journal/journal/admin.py
+++ fit_journal/journal/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from journal.models import Exercise, ExerciseSet, Training
+
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    """Админ для Exercise"""
+
+    list_display = ['name', 'translit']
+    readonly_fields = ['translit']
+
+
+@admin.register(ExerciseSet)
+class ExerciseSetAdmin(admin.ModelAdmin):
+    """Админ для ExerciseSet"""
+
+    list_display = ['exercise', 'training', 'repetition', 'weight']
+    raw_id_fields = ['exercise', 'training']
+
+
+@admin.register(Training)
+class TrainingAdmin(admin.ModelAdmin):
+    """Админ для Training"""
+
+    list_display = ['athlete', 'date']
+    raw_id_fields = ['athlete']
