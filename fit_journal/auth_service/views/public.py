02697da63f90cda51387cf748048cbd56dd17990
alex_barmen <www.lad282@gmail.com>
2025-11-22 22:22:19 +0300

    feat: update athlete register api view

--- fit_journal/auth_service/views/public.py
+++ fit_journal/auth_service/views/public.py
@@ -3,11 +3,12 @@ from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.serializers import TokenRefreshSerializer
+from rest_framework_simplejwt.serializers import TokenRefreshSerializer, TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from auth_service.models import Athlete
 from auth_service.permissions import HasNoRefreshToken, HasRefreshToken
-from auth_service.serializers import AccessTokenResponseSerializer
+from auth_service.serializers import AccessTokenResponseSerializer, RegisterAthleteRequestSerializer
 from auth_service.services import get_authenticated_response
 from utils.constants import APISchemaTags, DefaultAPIResponses
 
@@ -40,9 +41,10 @@ class HealthCheck(APIView):
         tags=[APISchemaTags.AUTH_SERVICE],
         summary='Регистрация спортсмена',
         operation_id='Регистрация спортсмена',
+        request=RegisterAthleteRequestSerializer,
         responses={
             **DefaultAPIResponses.RESPONSES,
-            status.HTTP_202_ACCEPTED: {},
+            status.HTTP_200_OK: AccessTokenResponseSerializer,
         },
     ),
 )
@@ -54,6 +56,33 @@ class RegisterAthlete(APIView):
 
     def post(self, request, *args, **kwargs):
         """POST-запрос"""
+        request_serializer = RegisterAthleteRequestSerializer(data=request.data)
+        request_serializer.is_valid(raise_exception=True)
+
+        new_athlete = Athlete.objects.create_user(
+            username=request_serializer.validated_data['username'],
+            password=request_serializer.validated_data['password'],
+            email=request_serializer.validated_data['email'],
+            last_name=request_serializer.validated_data['last_name'],
+            first_name=request_serializer.validated_data['first_name'],
+            second_name=request_serializer.validated_data['second_name'],
+            phone=request_serializer.validated_data['phone'],
+        )
+
+        token_serializer = TokenObtainPairSerializer(
+            data={
+                'username': request_serializer.validated_data['username'],
+                'password': request_serializer.validated_data['password'],
+            },
+        )
+        token_serializer.is_valid(raise_exception=True)
+
+        response = get_authenticated_response(
+            request,
+            token_serializer.validated_data['access'],
+            token_serializer.validated_data['refresh'],
+        )
+        return response
 
 
 @extend_schema_view(
