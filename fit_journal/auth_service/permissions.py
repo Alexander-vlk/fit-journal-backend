bfda1a1932c9d20dc0de780b0196fb345022313f
alex_barmen <www.lad282@gmail.com>
2025-11-22 21:12:00 +0300

    feat: has refresh token permission

--- fit_journal/auth_service/permissions.py
+++ fit_journal/auth_service/permissions.py
@@ -9,3 +9,11 @@ class HasNoRefreshToken(permissions.BasePermission):
     def has_permission(self, request, view):
         """Проверить наличие разрешения"""
         return not bool(request.COOKIES.get(REFRESH_TOKEN_COOKIE_NAME))
+
+
+class HasRefreshToken(permissions.BasePermission):
+    """Пользователь не имеет refresh-токена в cookie"""
+
+    def has_permission(self, request, view):
+        """Проверить наличие разрешения"""
+        return bool(request.COOKIES.get(REFRESH_TOKEN_COOKIE_NAME))
