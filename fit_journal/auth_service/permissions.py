bb29b25bb747d8b80fff89846d6aa97626318bd9
alex_barmen <www.lad282@gmail.com>
2025-11-22 12:21:49 +0300

    feat: permissions

---
+++ fit_journal/auth_service/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework import permissions
+
+from auth_service.constants import REFRESH_TOKEN_COOKIE_NAME
+
+
+class HasNoRefreshToken(permissions.BasePermission):
+    """Пользователь не имеет refresh-токена в cookie"""
+
+    def has_permission(self, request, view):
+        """Проверить наличие разрешения"""
+        return not bool(request.COOKIES.get(REFRESH_TOKEN_COOKIE_NAME))
