d8899b33f46be6f688a1ffa22c56893a011d3acc
alex_barmen <www.lad282@gmail.com>
2025-11-22 17:08:36 +0300

    feat: register api views in url

--- fit_journal/journal/urls.py
+++ fit_journal/journal/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-urlpatterns = [
+from journal.views import ExerciseList, ExerciseChangeView
 
+urlpatterns = [
+    path('journal/exercise/list', ExerciseList.as_view(), name='exercise_list'),
+    path('journal/exercise/create/', ExerciseChangeView.as_view(), name='create_exercise'),
 ]
