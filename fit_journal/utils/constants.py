7fa512e5c2248d1aad1d51add8dfd4379362755c
alex_barmen <www.lad282@gmail.com>
2025-11-22 17:07:45 +0300

    feat: update constants

--- fit_journal/utils/constants.py
+++ fit_journal/utils/constants.py
@@ -20,10 +20,10 @@ class DefaultAPIResponses:
     _internal_server_error_text = 'Произошла внутренняя ошибка сервера'
 
     RESPONSES = {
-        status.HTTP_200_OK: OpenApiResponse(description=_success_text),
         status.HTTP_400_BAD_REQUEST: OpenApiResponse(description=_bad_request_error_text),
         status.HTTP_401_UNAUTHORIZED: OpenApiResponse(description=_unauthorized_error_text),
         status.HTTP_403_FORBIDDEN: OpenApiResponse(description=_access_denied),
-        status.HTTP_405_METHOD_NOT_ALLOWED: OpenApiResponse(description=_not_found_error_text),
+        status.HTTP_404_NOT_FOUND: OpenApiResponse(description=_not_found_error_text),
+        status.HTTP_405_METHOD_NOT_ALLOWED: OpenApiResponse(description=_method_not_allowed_error_text),
         status.HTTP_500_INTERNAL_SERVER_ERROR: OpenApiResponse(description=_internal_server_error_text),
     }
