dc2715a586a862b787fdb33a9a0ecc1640c3d6c2
alex_barmen <www.lad282@gmail.com>
2025-11-29 11:51:39 +0300

    feat: color, training_type, athlete_trainingtype_color admin

--- fit_journal/journal/admin.py
+++ fit_journal/journal/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from journal.models import Exercise, ExerciseSet, Training
+from journal.models import Exercise, ExerciseSet, Training, Color, TrainingType, AthleteTrainingTypeColor
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    """Админ для Color"""
+
+    list_display = ['id', 'background_color', 'text_color']
 
 
 @admin.register(Exercise)
@@ -17,6 +24,21 @@ class ExerciseSetAdmin(admin.ModelAdmin):
 
     list_display = ['exercise', 'training', 'repetition', 'weight']
     raw_id_fields = ['exercise', 'training']
+
+
+@admin.register(TrainingType)
+class TrainingTypeAdmin(admin.ModelAdmin):
+    """Админ для TrainingType"""
+
+    list_display = ['name']
+
+
+@admin.register(AthleteTrainingTypeColor)
+class AthleteTrainingTypeColorAdmin(admin.ModelAdmin):
+    """Админ для AthleteTrainingTypeColor"""
+
+    list_display = ['id', 'athlete', 'training_type', 'color']
+    raw_id_fields =['athlete', 'training_type', 'color']
 
 
 @admin.register(Training)
