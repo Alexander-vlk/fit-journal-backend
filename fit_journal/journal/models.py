735d60695feffc01aca31110cadfa1b671ae4dcc
alex_barmen <www.lad282@gmail.com>
2025-11-29 11:36:14 +0300

    feat: color, training_type, athlete_trainingtype_color models

--- fit_journal/journal/models.py
+++ fit_journal/journal/models.py
@@ -1,10 +1,36 @@
 import re
 
+from django.core.validators import RegexValidator
 from django.db import models
 from transliterate import translit
 
 from auth_service.models import Athlete
 from utils.mixins import AutoDateMixin
+
+
+class Color(AutoDateMixin):
+    """Цвет"""
+
+    background_color = models.CharField(
+        verbose_name='Цвет фона',
+        help_text='TailwindCSS-стиль',
+        validators=[RegexValidator(regex=r'^bg-[a-z]+-\d+$')],
+        max_length=30,
+    )
+    text_color = models.CharField(
+        verbose_name='Цвет текста',
+        help_text='TailwindCSS-стиль',
+        validators=[RegexValidator(regex=r'^text-[a-z]+-\d+$')],
+        max_length=30,
+    )
+
+    class Meta:
+        verbose_name = 'Цветовая схема'
+        verbose_name_plural = 'Справочник цветовых схем'
+        unique_together = (('background_color', 'text_color'),)
+
+    def __str__(self):
+        return f'{self.background_color} {self.text_color}'
 
 
 class Exercise(AutoDateMixin):
@@ -37,10 +63,48 @@ class Exercise(AutoDateMixin):
         super().save(*args, **kwargs)
 
 
+class TrainingType(AutoDateMixin):
+    """Тип тренировки"""
+
+    name = models.CharField(verbose_name='Название', max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Тип тренировки'
+        verbose_name_plural = 'Справочник типов тренировок'
+        ordering = ['name']
+
+    def __str__(self):
+        """Строковое представление объекта модели"""
+        return self.name
+
+
+class AthleteTrainingTypeColor(AutoDateMixin):
+    """Связка цвет - тип тренировки для пользователя"""
+
+    athlete = models.ForeignKey(Athlete, on_delete=models.PROTECT)
+    color = models.ForeignKey(Color, on_delete=models.PROTECT)
+    training_type = models.ForeignKey(TrainingType, on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = 'Связка Спортсмен - Цвет - Тип тренировки'
+        verbose_name_plural = 'Связки Спортсмен - Цвет - Тип тренировки'
+
+    def __str__(self):
+        """Строковое представление объекта модели"""
+        return f'{self.athlete} - {self.color} - {self.training_type}'
+
+
 class Training(AutoDateMixin):
     """Тренировка"""
 
     athlete = models.ForeignKey(Athlete, verbose_name='Спортсмен', on_delete=models.PROTECT, related_name='trainings')
+    athlete_training_type = models.ForeignKey(
+        AthleteTrainingTypeColor,
+        on_delete=models.PROTECT,
+        related_name='trainings',
+        null=True,
+        blank=True,
+    )
     exercises = models.ManyToManyField(Exercise, verbose_name='Упражнения', related_name='trainings')
     date = models.DateField(verbose_name='Дата проведения', db_index=True)
 
